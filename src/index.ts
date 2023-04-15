b05c9cb13981f98de9f0aa8609977f0f6c03fae5
fouad5058 <fouad.bouk7@gmail.com>
2023-04-15 13:17:32 -0400

    fss calendrier qui marche

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+import type { Event} from './types'
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement=document.querySelector<HTMLDivElement>('[data-element="calendar"]')
+  if(!calendarElement) return;
+
+  const events = getEvent();
+  console.log({events});
+  
+  const calendar = new Calendar(calendarElement, {
+    plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek'
+    },
+    events,
+  });
+  calendar.render();  
 });
+
+
+const getEvent = (): Event[] => {
+  const scripts = document.querySelectorAll<HTMLScriptElement>('[data-element="event-data"]');
+  console.log({scripts});
+
+  const events = [...scripts].map((script)=> {
+    const event: Event = JSON.parse(script.textContent!);
+    event.start=new Date(event.start);
+    event.end=new Date(event.end);
+
+    return event ;
+  });
+  
+
+  return events;
+};
